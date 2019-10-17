473fb3522343f4366add91f90e341d300fecf6de
Kevin-Kip <masterfork5@gmail.com>
2019-10-17 17:08:52 +0300

    FIxed all operators but /

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -5,26 +5,26 @@
         <div @click="clear" class="btn">C</div>
         <div @click="sign" class="btn">&#177;</div>
         <div @click="percent" class="btn">%</div>
-        <div class="btn btn-special">&#xF7;</div>
+        <div @click="divide" class="btn btn-special">&#xF7;</div>
 
         <div @click="append('7')" class="btn">7</div>
         <div @click="append('8')" class="btn">8</div>
         <div @click="append('9')" class="btn">9</div>
-        <div class="btn btn-special">x</div>
+        <div @click="times" class="btn btn-special">x</div>
 
         <div @click="append('4')" class="btn">4</div>
         <div @click="append('5')" class="btn">5</div>
         <div @click="append('6')" class="btn">6</div>
-        <div class="btn btn-special">-</div>
+        <div @click="minus" class="btn btn-special">-</div>
 
         <div @click="append('1')" class="btn">1</div>
         <div @click="append('2')" class="btn">2</div>
         <div @click="append('3')" class="btn">3</div>
-        <div class="btn btn-special">+</div>
+        <div @click="add" class="btn btn-special">+</div>
 
         <div @click="append('0')" class="zero btn">0</div>
         <div @click="dot" class="btn">.</div>
-        <div class="btn btn-special">=</div>
+        <div @click="equals" class="btn btn-special">=</div>
     </div>
 </template>
 
@@ -34,6 +34,9 @@
         data() {
             return {
                 current: '',
+                previous: null,
+                operator: null,
+                operatorClicked: false
             }
         },
         methods: {
@@ -43,16 +46,44 @@
             sign() {
                 this.current = this.current.charAt(0) === '-' ? this.current.slice(1) : `-${this.current}`
             },
-            percent(){
+            percent() {
                 this.current = `${parseFloat(this.current) / 100}`
             },
-            append(num){
+            append(num) {
+                if (this.operatorClicked){
+                    this.current = '';
+                    this.operatorClicked = false;
+                }
                 this.current = `${this.current}${num}`
             },
-            dot(){
-                if (this.current.indexOf('.') === -1){
+            dot() {
+                if (this.current.indexOf('.') === -1) {
                     this.append('.')
                 }
+            },
+            setPrevious() {
+                this.previous = this.current;
+                this.operatorClicked = true
+            },
+            divide() {
+                this.operator = (a, b) => a / b;
+                this.setPrevious()
+            },
+            add() {
+                this.operator = (a, b) => a + b;
+                this.setPrevious()
+            },
+            minus() {
+                this.operator = (a, b) => a - b;
+                this.setPrevious()
+            },
+            times() {
+                this.operator = (a, b) => a * b;
+                this.setPrevious()
+            },
+            equals() {
+                this.current = `${this.operator(parseFloat(this.current), parseFloat(this.previous))}`;
+                this.previous = null
             }
         }
     }
